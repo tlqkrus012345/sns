7c34a32ad2351ff913a5fcc5392cd8ec897b4d3b
tlqkrus012345 <tlqkrus012345@naver.com>
2023-03-24 23:26:51 +0900

    회원 레포지토리 작성

---
+++ src/main/java/com/sns/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.sns.member.repository;
+
+import com.sns.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByMemberName(String memberName);
+}
