3de320946469ffdecf6c280ce21acebbe36b30e0
tlqkrus012345 <tlqkrus012345@naver.com>
2023-03-24 23:29:43 +0900

    postgres를 위한 yml 파일 작성

--- src/main/java/com/sns/SnsApplication.java
+++ src/main/java/com/sns/SnsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) // 데이터베이스 url 연결을 제외한다.
+@SpringBootApplication // 데이터베이스 url 연결을 제외한다. exclude = DataSourceAutoConfiguration.class
 public class SnsApplication {
 
 	public static void main(String[] args) {
