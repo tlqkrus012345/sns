19de1ee4d65eeb6b039aa138ced1f9214c676ed2
tlqkrus012345 <tlqkrus012345@naver.com>
2023-03-30 12:59:03 +0900

    게시판 삭제 기능 수정

--- src/main/java/com/sns/post/repository/CommentRepository.java
+++ src/main/java/com/sns/post/repository/CommentRepository.java
@@ -5,7 +5,15 @@ import com.sns.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAllByPost(Post post, Pageable pageable);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Comment o WHERE o.post = :post")
+    void deleteAllByPost(@Param("post") Post post);
 }
--- src/main/java/com/sns/post/repository/LikeRepository.java
+++ src/main/java/com/sns/post/repository/LikeRepository.java
@@ -4,8 +4,10 @@ import com.sns.member.entity.Member;
 import com.sns.post.entity.Like;
 import com.sns.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,4 +16,8 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByMemberAndPost(Member member, Post post);
     @Query(value = "SELECT COUNT(*) FROM Like l WHERE l.post =:post")
     Integer countByPost(@Param("post") Post post);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Like o WHERE o.post = :post")
+    void deleteAllByPost(@Param("post") Post post);
 }
